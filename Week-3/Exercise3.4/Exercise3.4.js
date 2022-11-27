0ef2b47bc1edbc5df571f5990a0d935db47a4e6a
himanshu_raj <heyhimanshur@gmail.com>
2022-11-27 11:13:40 +0530

    finished with ex-3.4

---
+++ Week-3/Exercise3.4/Exercise3.4.js
@@ -0,0 +1,25 @@
+function createStack() {
+  let items = [];
+  return function () {
+    return {
+      push(item) {
+        items.push(item);
+      },
+      pop(item) {
+        return items.pop(item);
+      },
+      getItems() {
+        console.log(items);
+      },
+    };
+  };
+}
+
+const getStack = createStack();
+const stack = getStack();
+console.log(getStack);
+stack.push(10);
+stack.push(5);
+stack.pop();
+console.log(stack.items); //undefined
+stack.getItems();
