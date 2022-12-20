0f39016825a68c92f69b2291d38a68196d78eb31
himanshu_raj <heyhimanshur@gmail.com>
2022-12-20 21:13:29 +0530

    custom pollyfills for promise

--- Week-4/Exercise4.1.js
+++ Week-4/Exercise4.1.js
@@ -1,5 +1,51 @@
+const State = {
+  PENDING: "pending",
+  FULFILLED: "fulfilled",
+  REJECTED: "rejected",
+};
+
+function CustomPromise(executor) {
+  let state = State.PENDING;
+  let value = null;
+  let handlers = [];
+  let catchers = [];
+
+  const resolve = (result) => {
+    if (state !== State.PENDING) return;
+    state = State.FULFILLED;
+    value = result;
+    handlers.forEach((h) => h(value));
+  };
+
+  const reject = (error) => {
+    if (state !== State.PENDING) return;
+    state = State.REJECTED;
+    value = error;
+    catchers.forEach((c) => c(value));
+  };
+
+  this.then = (successCallback) => {
+    if (state === State.FULFILLED) {
+      successCallback(value);
+    } else {
+      handlers.push(successCallback);
+    }
+    return this;
+  };
+
+  this.catch = (failureCallback) => {
+    if (state === State.REJECTED) {
+      failureCallback(value);
+    } else {
+      catchers.push(value);
+    }
+  };
+
+  executor(resolve, reject);
+}
+
 const getNumber = () =>
-  new Promise((resolve, reject) => {
+  new CustomPromise((resolve, reject) => {
     const random = parseInt(Math.random() * 100, 10);
     console.log(random);
     if (random % 5 === 0) {
